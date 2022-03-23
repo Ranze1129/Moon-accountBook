6ed64038a0d1d6ffe65c46d5a13c9181c1bd61c1
Ranze <1066467243@qq.com>
2022-03-23 14:59:49 +0800

    搞定三个 icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/tag.svg');
+require('icons/money.svg') ;
+require('icons/chart.svg');
+
+
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -11,7 +16,17 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.33333%;
       text-align: center;
-      padding: 16px;
+      display: flex;
+      padding: 4px 0;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      font-size: 14px;
+      .icon{
+        width: 24px;
+        height: 24px;
+        fill: #515151;
+      }
     }
   }
 `;
@@ -21,13 +36,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Link to="/tags">标签页</Link>
+          <svg className="icon">
+            <use xlinkHref="#tag"/>
+          </svg>
+          <Link to="/tags">标签</Link>
         </li>
         <li>
-          <Link to="/money">记账页</Link>
+          <svg className="icon">
+            <use xlinkHref="#money"/>
+          </svg>
+          <Link to="/money">记账</Link>
         </li>
         <li>
-          <Link to="/statistics">统计页</Link>
+          <svg className="icon">
+            <use xlinkHref="#chart"/>
+          </svg>
+          <Link to="/statistics">统计</Link>
         </li>
       </ul>
     </NavWrapper>
