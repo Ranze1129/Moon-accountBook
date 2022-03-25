51094afc9025a2f171365e33821db1a853edf215
Ranze <1066467243@qq.com>
2022-03-25 16:22:20 +0800

    收集表单数据

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React, {useRef, useState} from 'react';
+import React, {useRef} from 'react';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
@@ -12,7 +12,7 @@ const Wrapper = styled.section`
       margin-right: 16px;
       white-space: nowrap;
     }
-    >input{
+    > input {
       display: block;
       width: 100%;
       border: none;
@@ -22,15 +22,20 @@ const Wrapper = styled.section`
   }
 `;
 
-const NoteSection:React.FC= ()=>{
-  const [note, setNote] = useState('')
-  const refInput = useRef<HTMLInputElement>(null)
-  const onBlur = ()=>{
-    if(refInput.current !== null){
-      setNote(refInput.current.value)
+type Props = {
+  value: string;
+  onChange: (value: string) => void
+}
+
+const NoteSection: React.FC<Props> = (props) => {
+  const note = props.value;
+  const refInput = useRef<HTMLInputElement>(null);
+  const onBlur = () => {
+    if (refInput.current !== null) {
+      props.onChange(refInput.current.value);
     }
-  }
-  return(
+  };
+  return (
     <Wrapper>
       <label>
         <span>备注:</span>
