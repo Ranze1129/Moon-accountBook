1b549a692e8012280a51e92ee583d13f8bfb7ab1
Ranze <1066467243@qq.com>
2022-03-28 14:37:48 +0800

    创建单独的 Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+  > span {
+    margin-right: 16px;
+    white-space: nowrap;
+  }
+  > input {
+    display: block;
+    width: 100%;
+    border: none;
+    background: none;
+    height: 72px;
+  }
+`;
+
+
+type Props = {
+  label: string;
+} & React.InputHTMLAttributes<HTMLInputElement>
+
+const Input: React.FC<Props> = (props) => {
+  const {label,children, ...rest} = props;
+  return (
+    <Label>
+      <span>{props.label}</span>
+      <input {...rest}/>
+    </Label>
+  );
+};
+
+export {Input};
